85721309e912855e4ad323247f72dd7b07ce78b6
wuweishan <weishan@pengchatech>
2020-07-12 22:51:08 +0800

    *)增加注释

--- app/src/main/java/com/example/androidviewdemo/CameraView.java
+++ app/src/main/java/com/example/androidviewdemo/CameraView.java
@@ -11,6 +11,15 @@ import android.view.View;
 import androidx.annotation.NonNull;
 
 
+/**
+ * Camera的使用
+ * 调用Camera.applyToCanvas(Canvas)以当前画布的位置进行绘制后图形的投影效果
+ * 所以坐标系包含三个
+ * 图形显示的坐标系、Canvas的坐标系、Camera的坐标系---Canvas和Camera都是相对的坐标系，图形显示的才是绝对坐标系即屏幕的坐标系
+ * applyToCanvas，让Camera移动到Canvas坐标系的位置
+ * 图形绘制在Canvas相对坐标系下-》图形的绝对坐标系-》Camera的相对坐标系进行投影
+ * 所以applyToCanvas的意思是把Camera的坐标系移到和当前Canvas坐标系重合的位置吧？
+ */
 public class CameraView extends View {
     private Paint paint;
 
@@ -27,8 +36,10 @@ public class CameraView extends View {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
         camera = new Camera();
-        // 旋转45度
+        // 设置投影Z轴的位置，首先为Z轴的负值，负值越小投影位置越远，投影后的图形越小
+        // 单位为英寸，1英寸=72像素，需要使用转换为dp的值
         camera.setLocation(0, 0, - ScreenUtil.dp2Px(2));
+        // 旋转45度
         camera.rotateX(45);
     }
 

2dd5fb1c763977e88d9467febee40cc26757e0a2
wuweishan <weishan@pengchatech>
2020-07-28 07:54:42 +0800

    *)初始化第12章作业

---
+++ hencoder12_scalableimageview/src/main/java/com/example/hencoder12_scalableimageview/ScabableImageView.java
@@ -0,0 +1,13 @@
+package com.example.hencoder12_scalableimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class ScabableImageView extends View {
+    \public ScabableImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}

a03d7a179889cc7f18fc796ac9e263efaac659d2
wuweishan1995 <wws@gzqianyin.cn>
2020-07-29 16:04:52 +0800

    *)痛苦的第二章终于收工！

--- KotlinLeanLib/src/main/java/com/example/kotlinleanlib/MyClass.kt
+++ KotlinLeanLib/src/main/java/com/example/kotlinleanlib/MyClass.kt
@@ -1,11 +1,18 @@
 package com.example.kotlinleanlib
 
 fun  main(args : Array<String>){
-    val test = Test()
-    test.evalWithLogging(Test.Sum(Test.Sum(Test.Num(1), Test.Num(2)), Test.Num(3)));
+//    val test = Test()
+//    test.evalWithLogging(Test.Sum(Test.Sum(Test.Num(1), Test.Num(2)), Test.Num(3)));
 //    test.testFizzBuzz()
 //    test.testFizzBuzzDownTo()
-    test.testForByUtil()
+//    test.testForByUtil()
+
+    val mapTet = MapTest()
+//    mapTet.execute()
+//    mapTet.forWithIndex()
+    println(mapTet.recognize('1'))
+    println(mapTet.recognize('A'))
+    println(mapTet.recognize('@'))
 }
 
 class MyClass {

15e4fd959e52d391cc855e456e1ca92361f1630a
wuweishan1995 <wws@gzqianyin.cn>
2020-08-19 19:30:05 +0800

    *)auto merge

---
+++ KotlinLeanLib/src/main/java/com/example/coreprogramming/lesson5/Seat.kt
@@ -0,0 +1,11 @@
+﻿package com.example.coreprogramming.lesson5
+
+// 学生可为空
+data class Seat(val student: Student?)
+// 度数可为空
+data class Student(val glasses: Glasses?)
+data class Glasses(val degreeOfMyopia : Double)
+
+fun getDegree(seat: Seat) :Double{
+    return seat.student?.glasses?.degreeOfMyopia?: 0.0
+}
---
+++ KotlinLeanLib/src/main/java/com/example/coreprogramming/lesson5/StyleDemo.kt
@@ -0,0 +1,5 @@
+﻿package com.example.coreprogramming.lesson5
+
+fun  main(args : Array<String>){
+
+}

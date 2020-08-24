4009dabec8a9324aede32a763ffcd4a0592ccbb7
wuweishan1995 <wws@gzqianyin.cn>
2020-08-24 10:39:22 +0800

    *)优化SnapHelper

--- nestedscrolldemo/src/main/java/com/example/nestedscrolldemo/IconRepo.java
+++ nestedscrolldemo/src/main/java/com/example/nestedscrolldemo/IconRepo.java
@@ -29,6 +29,33 @@ public class IconRepo {
 
         iconBean = new IconBean(R.drawable.p1135463, "录音机");
         sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.t1135335, "Android机器人");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.t1135338, "记事本");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.t1135341, "谷歌地图");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.t1135335, "Android机器人");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.t1135338, "记事本");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.t1135341, "谷歌地图");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.p1135453, "钥匙串");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.p1135462, "播放器");
+        sIconBeanList.add(iconBean);
+
+        iconBean = new IconBean(R.drawable.p1135463, "录音机");
+        sIconBeanList.add(iconBean);
         return sIconBeanList;
     }
 }
--- nestedscrolldemo/src/main/java/com/example/nestedscrolldemo/snaphelper/GallerySnapHelper.java
+++ nestedscrolldemo/src/main/java/com/example/nestedscrolldemo/snaphelper/GallerySnapHelper.java
@@ -2,12 +2,14 @@ package com.example.nestedscrolldemo.snaphelper;
 
 import android.content.IntentFilter;
 import android.graphics.PointF;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
 import androidx.recyclerview.widget.OrientationHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SnapHelper;
@@ -22,8 +24,54 @@ import androidx.recyclerview.widget.SnapHelper;
  */
 public class GallerySnapHelper extends SnapHelper {
     private static final float INVALID_DISTANCE = 1f;
+    // fling时滑过的最大item数：3个，防止一滑，滑到底的情况
+    private static final int MAX_FLING_SIZE = 3;
+    // 每像素滑动需要的时间：通过该值控制Fling时的快慢
+    private static final float MILLISECONDS_PER_INCH = 25f;
     private OrientationHelper mHelper;
+    private RecyclerView mRecyclerView;
     public GallerySnapHelper(){
+    }
+
+    @Override
+    public void attachToRecyclerView(@Nullable RecyclerView recyclerView) throws IllegalStateException {
+        mRecyclerView = recyclerView;
+        super.attachToRecyclerView(recyclerView);
+    }
+
+    // 找当前显示内容下需要定位的itemView
+    @Nullable
+    @Override
+    public View findSnapView(RecyclerView.LayoutManager layoutManager) {
+        Log.i("WWS", "findSnapView");
+        return findStartView(layoutManager, getHorizontalHelper(layoutManager));
+    }
+
+    private View findStartView(RecyclerView.LayoutManager layoutManager, OrientationHelper orientationHelper){
+        Log.i("WWS", "findStartView");
+        if (layoutManager instanceof LinearLayoutManager){
+            // 找到第一个可见的
+            int firstChildPos = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+            // 如果没找到，返回空
+            if (firstChildPos == RecyclerView.NO_POSITION){
+                return null;
+            }
+            // 如果已经滑倒最后面了，就不用重定位了
+            if (((LinearLayoutManager) layoutManager).findLastCompletelyVisibleItemPosition() == (layoutManager.getItemCount() - 1)){
+                return null;
+            }
+            // 如果第一个View显示超过了一半，就以它来定位，否则以下一个item来定位
+            View firstChildView = layoutManager.findViewByPosition(firstChildPos);
+            if (orientationHelper.getDecoratedEnd(firstChildView) >= orientationHelper.getDecoratedMeasurement(firstChildView) / 2
+                //这个条件好像没必要，能看到，肯定是正的吧
+//                && mHelper.getDecoratedEnd(firstChildView) > 0
+            ){
+                return firstChildView;
+            }else{
+                return layoutManager.findViewByPosition(firstChildPos + 1);
+            }
+        }
+        return null;
     }
 
     /**
@@ -50,47 +98,38 @@ public class GallerySnapHelper extends SnapHelper {
         return mHelper;
     }
 
-
     // itemView距离起始位置的距离
     private int distanceToStart(View targetView, OrientationHelper helper){
         Log.i("WWS", "distanceToStart start = " + (helper.getDecoratedStart(targetView)) + " startAfterPadding = " + helper.getStartAfterPadding());
         return helper.getDecoratedStart(targetView) - helper.getStartAfterPadding();
     }
 
-    // 找当前显示内容下需要定位的itemView
     @Nullable
     @Override
-    public View findSnapView(RecyclerView.LayoutManager layoutManager) {
-        Log.i("WWS", "findSnapView");
-        return findStartView(layoutManager, getHorizontalHelper(layoutManager));
-    }
-
-    // 找最终停的snapView
-    private View findStartView(RecyclerView.LayoutManager layoutManager, OrientationHelper orientationHelper){
-        Log.i("WWS", "findStartView");
-        if (layoutManager instanceof LinearLayoutManager){
-            // 找到第一个可见的
-            int firstChildPos = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
-            // 如果没找到，返回空
-            if (firstChildPos == RecyclerView.NO_POSITION){
-                return null;
-            }
-            // 如果已经滑倒最后面了，就不用重定位了
-            if (((LinearLayoutManager) layoutManager).findLastCompletelyVisibleItemPosition() == (layoutManager.getItemCount() - 1)){
-                return null;
-            }
-            // 如果第一个View显示超过了一半，就以它来定位，否则以下一个item来定位
-            View firstChildView = layoutManager.findViewByPosition(firstChildPos);
-            if (orientationHelper.getDecoratedEnd(firstChildView) >= orientationHelper.getDecoratedMeasurement(firstChildView) / 2
-                     //这个条件好像没必要，能看到，肯定是正的吧
-//                && mHelper.getDecoratedEnd(firstChildView) > 0
-            ){
-                return firstChildView;
-            }else{
-                return layoutManager.findViewByPosition(firstChildPos + 1);
-            }
+    protected RecyclerView.SmoothScroller createScroller(@NonNull RecyclerView.LayoutManager layoutManager) {
+        if (mRecyclerView == null){
+            return null;
         }
-        return null;
+        if (!(layoutManager instanceof RecyclerView.SmoothScroller.ScrollVectorProvider)) {
+            return null;
+        }
+        return new LinearSmoothScroller(mRecyclerView.getContext()) {
+            @Override
+            protected void onTargetFound(View targetView, RecyclerView.State state, Action action) {
+                int[] snapDistances = calculateDistanceToFinalSnap(mRecyclerView.getLayoutManager(), targetView);
+                final int dx = snapDistances[0];
+                final int dy = snapDistances[1];
+                final int time = calculateTimeForDeceleration(Math.max(Math.abs(dx), Math.abs(dy)));
+                if (time > 0) {
+                    action.update(dx, dy, time, mDecelerateInterpolator);
+                }
+            }
+
+            @Override
+            protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
+                return MILLISECONDS_PER_INCH / displayMetrics.densityDpi;
+            }
+        };
     }
 
     /**
@@ -135,7 +174,12 @@ public class GallerySnapHelper extends SnapHelper {
         if (deltaJump == 0){
             return RecyclerView.NO_POSITION;
         }
-
+        Log.i("WWS", "deltaJump = " + deltaJump);
+        if (deltaJump > 0 && deltaJump >= MAX_FLING_SIZE){
+            deltaJump = MAX_FLING_SIZE;
+        } else if (deltaJump < 0 && deltaJump <= (-MAX_FLING_SIZE)) {
+            deltaJump = -MAX_FLING_SIZE;
+        }
         int targetPos = currentPos + deltaJump;
         if (targetPos < 0){
             targetPos = 0;

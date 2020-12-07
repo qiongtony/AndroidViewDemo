d444828fb37741250be16d84007823c574e0f49c
wuweishan1995 <wws@gzqianyin.cn>
2020-12-07 11:53:52 +0800

    *）优化动态改变列表高度的实现

---
+++ adaptivelayoutdemo/src/main/java/com/example/adaptivelayoutdemo/AdaptiveHelper.java
@@ -0,0 +1,109 @@
+package com.example.adaptivelayoutdemo;
+
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.NestedScrollingParentHelper;
+import androidx.core.view.ViewCompat;
+
+public class AdaptiveHelper {
+    private int mMinHeight;
+    private int mMaxHeight;
+    /**
+     * 是否开启功能
+     */
+    private boolean open = true;
+    /**
+     * 是否需要变短
+     */
+    private boolean needShorten = false;
+    private NestedScrollingParentHelper mHelper;
+    private View mTargetView;
+
+    public AdaptiveHelper(ViewGroup root, int maxHeight, boolean needShorten) {
+        mMaxHeight = maxHeight;
+        this.needShorten = needShorten;
+        mHelper = new NestedScrollingParentHelper(root);
+    }
+
+    public void setMinHeight(int minHeight) {
+        mMinHeight = minHeight;
+    }
+
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0 && open;
+    }
+
+
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
+        mHelper.onNestedScrollAccepted(child, target, axes, type);
+    }
+
+    public void onStopNestedScroll(@NonNull View target, int type) {
+        mHelper.onStopNestedScroll(target, type);
+    }
+
+    // 列表滑完了，剩余的让父View消费
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        Log.w(getClass().getSimpleName(), "onNestedScroll dyUnconsumed = " + dyUnconsumed + " dyConsumed = " + dyConsumed);
+        if (mTargetView == null) {
+            return;
+        }
+        // 下滑,等列表到顶后尺寸再变大
+        if (dyUnconsumed < 0) {
+            ViewGroup.LayoutParams layoutParams = mTargetView.getLayoutParams();
+            int height = mTargetView.getMeasuredHeight();
+            // 没到最大值，变大
+            if (height < mMaxHeight) {
+                // 求消费的
+                int changedHeight = -Math.min(mMaxHeight - height, Math.abs(dyUnconsumed) * 2);
+                Log.w(getClass().getSimpleName(), "onNestedScroll changedHeight = " + changedHeight + " height = " + layoutParams.height);
+                layoutParams.height = height - changedHeight;
+                mTargetView.setLayoutParams(layoutParams);
+            }
+        } else {
+            /*View containerView = findViewById(R.id.container_view);
+            ViewGroup.LayoutParams layoutParams = containerView.getLayoutParams();
+            int height = containerView.getMeasuredHeight();
+            if (height > TARGET_MIN_HEIGHT) {
+                int increasedHeight = Math.min(height - TARGET_MIN_HEIGHT, Math.abs(dyUnconsumed));
+                layoutParams.height = height - increasedHeight;
+                containerView.setLayoutParams(layoutParams);
+            }*/
+        }
+    }
+
+    // 列表还没开始滑，父View根据需求判断需不需要滑，并求出剩余子View可滑的距离
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        Log.w(getClass().getSimpleName(), "onNestedPreScroll dy = " + dy);
+        if (mTargetView == null) {
+            return;
+        }
+        // 下滑
+        if (dy <= 0) {
+        } else {
+            // 上滑，先处理，自己先处理，尺寸变小后再让RV处理
+            if (needShorten) {
+                ViewGroup.LayoutParams layoutParams = mTargetView.getLayoutParams();
+                int height = mTargetView.getMeasuredHeight();
+                if (height > mMinHeight) {
+                    int increasedHeight = Math.min(height - mMinHeight, Math.abs(dy));
+                    consumed[1] = increasedHeight >= dy ? dy : increasedHeight;
+                    Log.w(getClass().getSimpleName(), "onNestedPreScroll changedHeight = " + consumed[1] + " height = " + layoutParams.height);
+                    layoutParams.height = height - increasedHeight;
+                    mTargetView.setLayoutParams(layoutParams);
+                }
+            }
+        }
+    }
+
+    public void setTargetView(View targetView) {
+        mTargetView = targetView;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+}
--- adaptivelayoutdemo/src/main/java/com/example/adaptivelayoutdemo/CustomConstraintLayout.java
+++ adaptivelayoutdemo/src/main/java/com/example/adaptivelayoutdemo/CustomConstraintLayout.java
@@ -4,14 +4,11 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.NestedScrollingParent2;
-import androidx.core.view.NestedScrollingParentHelper;
-import androidx.core.view.ViewCompat;
 
 /**
  *
@@ -19,12 +16,12 @@ import androidx.core.view.ViewCompat;
 public class CustomConstraintLayout extends ConstraintLayout implements
         // 用NestedScrollingParent3,有两个onNestedScroll，长的那个才生效？
         NestedScrollingParent2 {
-    private NestedScrollingParentHelper mHelper;
-    private int TARGET_MIN_HEIGHT = ScreenUtils.dp2px(300);
-    private int TARGET_MAX_HEIGHT = ScreenUtils.dp2px(400);
+    private int TARGET_MIN_HEIGHT = ScreenUtils.dp2px(238);
+    private int TARGET_MAX_HEIGHT = ScreenUtils.dp2px(432);
+    private AdaptiveHelper mAdaptiveHelper;
 
     {
-        mHelper = new NestedScrollingParentHelper(this);
+        mAdaptiveHelper = new AdaptiveHelper(this, TARGET_MAX_HEIGHT, false);
     }
 
     public CustomConstraintLayout(@NonNull Context context) {
@@ -51,74 +48,43 @@ public class CustomConstraintLayout extends ConstraintLayout implements
     // 竖向滑动才处理嵌套滑动
     @Override
     public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
-        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+        return mAdaptiveHelper.onStartNestedScroll(child, target, axes, type);
     }
 
     @Override
     public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
-        mHelper.onNestedScrollAccepted(child, target, axes, type);
+        mAdaptiveHelper.onNestedScrollAccepted(child, target, axes, type);
     }
 
     @Override
     public void onStopNestedScroll(@NonNull View target, int type) {
-        mHelper.onStopNestedScroll(target, type);
+        mAdaptiveHelper.onStopNestedScroll(target, type);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mAdaptiveHelper.setTargetView(findViewById(R.id.container_view));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        mAdaptiveHelper.setTargetView(findViewById(R.id.container_view));
     }
 
     // 列表滑完了，剩余的让父View消费
     @Override
     public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
-        Log.w(getClass().getSimpleName(), "onNestedScroll dyUnconsumed = " + dyUnconsumed);
-        // 下滑,等列表到顶后尺寸再变大
-        if (dyUnconsumed <= 0) {
-            View containerView = findViewById(R.id.container_view);
-            ViewGroup.LayoutParams layoutParams = containerView.getLayoutParams();
-            int height = containerView.getMeasuredHeight();
-            // 没到最大值，变大
-            if (height < TARGET_MAX_HEIGHT) {
-                // 求消费的
-                int changedHeight = -Math.min(TARGET_MAX_HEIGHT - height, Math.abs(dyUnconsumed) * 2);
-                layoutParams.height = height - changedHeight;
-                containerView.setLayoutParams(layoutParams);
-            }
-        } else {
-            /*View containerView = findViewById(R.id.container_view);
-            ViewGroup.LayoutParams layoutParams = containerView.getLayoutParams();
-            int height = containerView.getMeasuredHeight();
-            if (height > TARGET_MIN_HEIGHT) {
-                int increasedHeight = Math.min(height - TARGET_MIN_HEIGHT, Math.abs(dyUnconsumed));
-                layoutParams.height = height - increasedHeight;
-                containerView.setLayoutParams(layoutParams);
-            }*/
-        }
+        Log.w(getClass().getSimpleName(), "onNestedScroll dyUnconsumed = " + dyUnconsumed + " dyConsumed = " + dyConsumed);
+        mAdaptiveHelper.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
     }
 
     // 列表还没开始滑，父View根据需求判断需不需要滑，并求出剩余子View可滑的距离
     @Override
     public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
         Log.w(getClass().getSimpleName(), "onNestedPreScroll dy = " + dy);
-        // 下滑
-        if (dy <= 0) {
-           /* View containerView = findViewById(R.id.container_view);
-            ViewGroup.LayoutParams layoutParams = containerView.getLayoutParams();
-            int height = containerView.getMeasuredHeight();
-            // 没到最大值，变大
-            if (height < TARGET_MAX_HEIGHT){
-                // 求消费的
-                consumed[1] = -Math.min(TARGET_MAX_HEIGHT - height, Math.abs(dy));
-                layoutParams.height = height - consumed[1];
-                containerView.setLayoutParams(layoutParams);
-            }*/
-        } else {
-            // 上滑，先处理，自己先处理，尺寸变小后再让RV处理
-            View containerView = findViewById(R.id.container_view);
-            ViewGroup.LayoutParams layoutParams = containerView.getLayoutParams();
-            int height = containerView.getMeasuredHeight();
-            if (height > TARGET_MIN_HEIGHT) {
-                int increasedHeight = Math.min(height - TARGET_MIN_HEIGHT, Math.abs(dy) * 2);
-                consumed[1] = increasedHeight >= dy ? dy : increasedHeight;
-                layoutParams.height = height - increasedHeight;
-                containerView.setLayoutParams(layoutParams);
-            }
-        }
+        mAdaptiveHelper.onNestedPreScroll(target, dx, dy, consumed, type);
     }
 }
